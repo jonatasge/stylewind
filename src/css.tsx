7815ba5ad37e226deafd39e7d93797a05270a122
Jonatas Evangelista <jonatasge@gmail.com>
2024-02-18 22:59:31 -0300

    fix: css function

--- src/css.tsx
+++ src/css.tsx
@@ -1,14 +1,18 @@
 export function css(
   ...values: (string | number | TemplateStringsArray | CallableFunction)[]
 ) {
-  return values.reduce((r, val) => {
-    const type = typeof val;
+  if (values[0] instanceof Array && typeof values[0][0] === "string") {
+    return values[0].reduce(
+      (r, val, i) => `${r}${val}${handleTemplate(values[i + 1])}`,
+      ""
+    );
+  }
+  return "";
+}
 
-    if (val instanceof Array && typeof val[0] === "string")
-      return val.join(" ");
-    if (type === "function") return `${r} ${(val as CallableFunction)()}`;
-    if (["number", "string"].includes(type)) return `${r} ${val}`;
-
-    return r;
-  }, "") as string;
+function handleTemplate(value: unknown) {
+  const type = typeof value;
+  if (type === "function") return (value as CallableFunction)();
+  if (["number", "string"].includes(type)) return value;
+  return "";
 }
